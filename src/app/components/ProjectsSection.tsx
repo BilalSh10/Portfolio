c38ba983cfcbc189d4e263fe289325e5e5af53cb
Bilal Shweike <81863952+BilalSh10@users.noreply.github.com>
2024-01-01 22:17:52 +0200

    add the key property

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -19,37 +19,44 @@ const ProjectsSection = () => {
         </h1>
       </div>
       <div className="flex flex-col justify-center items-center border gap-20 p-16 pb-24 phone:pb-16 phone:p-10">
-        {projects.map((project) => {
+        {projects.map((project, index) => {
           return (
-            <>
-              <div className="flex flex-col items-center justify-center gap-6 shadow-xl border p-6 rounded-xl w-2/3 h-auto phone:w-72">
-                <div className="flex flex-row justify-between w-full items-center phone:order-last phone:flex-col phone:gap-6">
-                  <h1 className="font-semibold phone:text-base phone:font-bold">
-                    {project.name}
-                  </h1>
-                  <span className="phone:hidden">|</span>
-                  <div className="flex flex-row gap-8 items-center phone:gap-12">
-                    <Link
-                      target="_blank"
-                      href={project.website_url}
-                      className="inline-flex items-center justify-center h-8 px-4 font-medium text-sm tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none"
-                    >
-                      Visit Project
-                    </Link>
-                    <a
-                      className="hover:text-neutral-800 transition-all duration-100"
-                      target="_blank"
-                      href={project.github_url}
-                    >
-                      <BsGithub size={20} />
-                    </a>
-                  </div>
-                </div>
-                <div>
-                  <Image className="rounded-lg" src={project.image} width={900} height={400} alt="pic" />
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center gap-6 shadow-xl border p-6 rounded-xl w-2/3 h-auto phone:w-72"
+            >
+              <div className="flex flex-row justify-between w-full items-center phone:order-last phone:flex-col phone:gap-6">
+                <h1 className="font-semibold phone:text-base phone:font-bold">
+                  {project.name}
+                </h1>
+                <span className="phone:hidden">|</span>
+                <div className="flex flex-row gap-8 items-center phone:gap-12">
+                  <Link
+                    target="_blank"
+                    href={project.website_url}
+                    className="inline-flex items-center justify-center h-8 px-4 font-medium text-sm tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none"
+                  >
+                    Visit Project
+                  </Link>
+                  <a
+                    className="hover:text-neutral-800 transition-all duration-100"
+                    target="_blank"
+                    href={project.github_url}
+                  >
+                    <BsGithub size={20} />
+                  </a>
                 </div>
               </div>
-            </>
+              <div>
+                <Image
+                  className="rounded-lg"
+                  src={project.image}
+                  width={900}
+                  height={400}
+                  alt="pic"
+                />
+              </div>
+            </div>
           );
         })}
       </div>
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -17,14 +17,12 @@ const Skills = () => {
       <div className="grid grid-cols-5 grid-rows-2 gap-x-7 gap-y-11 border border-gray-400 rounded-xl shadow-xl p-16 phone:grid-cols-2 phone:p-0 phone:pl-6 phone:pr-6 phone:gap-y-0 phone:shadow-none phone:border-none">
         {skillsData.map((skill) => {
           return (
-            <>
-              <div className="flex flex-col items-center gap-4 hover:shadow-lg p-12 hover:rounded-xl">
-                <div className="w-28 h-28">
-                  <Image src={skill.image} width={200} height={200} alt="pic" />
-                </div>
-                <h1 className="font-medium">{skill.name}</h1>
+            <div key={skill.name} className="flex flex-col items-center gap-4 hover:shadow-lg p-12 hover:rounded-xl">
+              <div className="w-28 h-28">
+                <Image src={skill.image} width={200} height={200} alt="pic" />
               </div>
-            </>
+              <h1 className="font-medium">{skill.name}</h1>
+            </div>
           );
         })}
       </div>
